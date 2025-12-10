cfc924e40d6f80b0d04f44d3736fa5c5220ff785
Alex <X00197973@mytudublin.ie>
2025-12-10 18:58:09 +0000

    Actually get rid of Server and Shared projects

--- Shared/FrankResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared
-{
-    public class FrankResponse
-    {
-        public string Base { get; set; }
-        public Dictionary<string, decimal> Rates { get; set; }
-        public string Date {  get; set; }
-    }
-}
